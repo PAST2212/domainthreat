a766d7199124f0bcbdaa23112435c5ae67c812ad
Patrick Steinhoff <124390875+PAST2212@users.noreply.github.com>
2024-04-14 11:38:12 +0200

    Update rapiddns.py

--- domainthreat/recon/rapiddns.py
+++ domainthreat/recon/rapiddns.py
@@ -5,7 +5,7 @@ from aiolimiter import AsyncLimiter
 import asyncio
 from bs4 import BeautifulSoup
 import json
-from core.webscraper import HtmlContent
+from domainthreat.core.webscraper import HtmlContent
 
 class ScanerRapidDns:
     def __init__(self) -> None:
